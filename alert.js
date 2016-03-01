4b5f6584619c15d36872f1e2a018fd53abde2579
Serg <Sergik148@gmail.com>
2016-03-01 21:48:18 +0200

    ed

--- alert.js
+++ alert.js
@@ -1,15 +1,16 @@
 var value = prompt('Кто пришел?', '');
 if (value == "админ") {
-  var password = prompt('Пароль?', '');
-  if (password == "Чёрный Властелин") {
-  	alert("Добро пожаловать!");
-} else if (password == null) {
-	alert("Вход отменён");
-}else{
-	alert('Пароль неверен');
+ 	 var password = prompt('Пароль?', '');
+  	if (password == "Чёрный Властелин") {
+  		alert("Добро пожаловать!");
+	} else if (password == null) {
+		alert("Вход отменён");
+	} else {
+		alert('Пароль неверен');
 } 
 } else if (value == null) {
 	alert("Вход отменён");
 } else {
   alert( "Я вас не знаю" );
 }
+// ed
