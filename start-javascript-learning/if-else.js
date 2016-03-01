644b9c6b2a94d06966c51da5d6fd3e55fd235923
vlad <vlad@datanrg.com>
2016-03-01 22:27:16 +0200

    fixed-projects-structure

--- start-javascript-learning/if-else.js
+++ start-javascript-learning/if-else.js
@@ -1,16 +1,15 @@
 var value = prompt('Кто пришел?', '');
 if (value == "админ") {
- 	 var password = prompt('Пароль?', '');
-  	if (password == "Чёрный Властелин") {
+	var password = prompt('Пароль?', '');
+	if (password == "Чёрный Властелин") {
   		alert("Добро пожаловать!");
 	} else if (password == null) {
 		alert("Вход отменён");
 	} else {
 		alert('Пароль неверен');
-} 
+	}
 } else if (value == null) {
 	alert("Вход отменён");
 } else {
-  alert( "Я вас не знаю" );
+	alert( "Я вас не знаю" );
 }
-// ed
